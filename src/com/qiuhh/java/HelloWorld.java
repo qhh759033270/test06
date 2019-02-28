85c56e7a03f76c8eadd7a44e78484f9682d152d9
qhh759033270 <759033270@qq.com>
2019-02-28 17:48:24 +0800

    默认的

--- src/com/qiuhh/java/HelloWorld.java
+++ src/com/qiuhh/java/HelloWorld.java
@@ -1,10 +1,5 @@
 package com.qiuhh.java;
 
-import com.module01.java.HelloWorld2;
-
-import java.util.ArrayList;
-import java.util.Date;
-
 /**
  * Created by xiaohao on 2019/2/28.
  */
@@ -15,32 +10,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("hello world");
-
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 11; j++) {
-
-            }
-        }
-
-        new ArrayList();
-
-        ArrayList xx = new ArrayList();
-        if (xx != null) {
-
-        }
-
-
-
-        HelloWorld2 xx1=new HelloWorld2();
-
-        ArrayList<String> list2 = new ArrayList<String>();
-        for (int i = 0; i < 10; i++) {
-
-        }
-        list2.add("123");
-        Date date = new Date();
-
-        xxx();
+        System.out.println("hello world");
     }
 
     public static void  xxx() {
