1871f3845820c28df1bca5884882a3192856266d
qhh759033270 <759033270@qq.com>
2019-02-28 18:22:49 +0800

    默认的

--- src/com/qiuhh/java/HelloWorld.java
+++ src/com/qiuhh/java/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.qiuhh.java;
 
+import com.sun.org.apache.xpath.internal.operations.String;
+
 /**
  * Created by xiaohao on 2019/2/28.
  */
@@ -11,6 +13,14 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+
+        String string = new String();
+
     }
 
     public static void  xxx() {
