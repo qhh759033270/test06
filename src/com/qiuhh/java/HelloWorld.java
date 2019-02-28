da03da3b19b811585b3356be0c5719723f245976
qhh759033270 <759033270@qq.com>
2019-02-28 18:31:47 +0800

    默认的

--- src/com/qiuhh/java/HelloWorld.java
+++ src/com/qiuhh/java/HelloWorld.java
@@ -1,16 +1,17 @@
 package com.qiuhh.java;
 
-import com.sun.org.apache.xpath.internal.operations.String;
+
+import java.util.ArrayList;
 
 /**
  * Created by xiaohao on 2019/2/28.
  */
 public class HelloWorld {
 
-
-    private String abc;
-
     public static void main(String[] args) {
+
+        ArrayList arrayList = new ArrayList();
+
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
@@ -19,7 +20,7 @@ public class HelloWorld {
         System.out.println("hello world");
         System.out.println("hello world");
 
-        String string = new String();
+
 
     }
 
